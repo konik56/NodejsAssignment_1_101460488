3cc884a8b8a73da5d9740a37e2c2458dbe4693f4
Illia Konik <illia.konik.2004@gmail.com>
2024-10-06 12:34:15 -0400

    move .evn to docker compose

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose
   })
   .then(() => {
     console.log("MongoDB connected");
-    const PORT = process.env.PORT || 5001;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => {
